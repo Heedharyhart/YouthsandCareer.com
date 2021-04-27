5011ea5b54ef60652b01cf297f6b1f5aef3458c4
Abdulkareem <idayatolaide0@gmail.com>
2021-04-27 17:41:37 +0100

    Remove the engine in package.json

---
+++ youthsandcareers-website/src/Pages/Login/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import './style.css';
+import SignIn from "./../../components/SignIn"
+
+class Login extends Component {
+    render(){
+        return (<SignIn/>)
+    }
+   
+}
+
+
+export default Login;
